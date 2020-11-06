1a8a523de7635852c294c8ad57a6ee811bc474ae
Vincenzo Madaghiele <vincenzo.madaghiele@gmail.com>
2020-11-06 15:50:27 +0100

    added: eval and other

--- pitch_durationTRANSFORMER.py
+++ pitch_durationTRANSFORMER.py
@@ -134,6 +134,7 @@ if __name__ == '__main__':
     X_pitch = datasetPitch.getData()
     # set vocabulary for conversion
     vocabPitch = datasetPitch.vocab
+    # Add padding tokens to vocab
     vocabPitch.append('<pad>')
     vocabPitch.append('<sos>')
     vocabPitch.append('<eos>')
@@ -150,6 +151,7 @@ if __name__ == '__main__':
     X_duration = datasetDuration.getData()
     # set vocabulary for conversion
     vocabDuration = datasetDuration.vocab
+    # Add padding tokens to vocab
     vocabDuration.append('<pad>')
     vocabDuration.append('<sos>')
     vocabDuration.append('<eos>')
@@ -161,8 +163,9 @@ if __name__ == '__main__':
     val_duration = X_duration[int(len(X_duration)*0.7)+1:int(len(X_duration)*0.7)+1+int(len(X_duration)*0.1)]
     test_duration = X_duration[int(len(X_duration)*0.7)+1+int(len(X_duration)*0.1):]
     
-    #%% DATA PREPARATION
     
+    
+    #%% DATA PREPARATION
 
     # pad data to max_lenght of sequences, prepend <sos> and append <eos>
     def pad(data):
@@ -185,20 +188,8 @@ if __name__ == '__main__':
         lengths.append(len(padded[-1]) - max(0, max_len - len(x)))
         return padded
     
-    
     # divide into batches of size bsz and converts notes into numbers
     def batchify(data, bsz, dict_to_ix):
-        
-        """
-        new_data = []
-        for sequence in data:
-            for note in sequence:
-                new_data.append(note)
-        new_data = np.array(new_data)
-        
-        
-        data = torch.tensor([dict_to_ix[w] for w in new_data], dtype=torch.long)
-        """
         
         padded = pad(data)
         padded_num = [[dict_to_ix[x] for x in ex] for ex in padded]
@@ -253,7 +244,7 @@ if __name__ == '__main__':
     
     # TRAIN AND EVALUATE LOSS
     best_val_loss = float("inf")
-    epochs = 10 # The number of epochs
+    epochs = 100 # The number of epochs
     best_model = None
     
     # TRAINING LOOP
@@ -280,6 +271,10 @@ if __name__ == '__main__':
         test_loss, math.exp(test_loss)))
     print('=' * 89)
     
+    savePATHpitch = 'modelsPitch/modelPitch_'+ str(epochs) + 'epochs_padding.pt'
+    state_dictPitch = modelPitch.state_dict()
+    torch.save(state_dictPitch, savePATHpitch)
+    
 
     #%% DURATION MODEL TRAINING
     
@@ -300,7 +295,7 @@ if __name__ == '__main__':
     
     # TRAIN AND EVALUATE LOSS
     best_val_loss = float("inf")
-    epochs = 10 # The number of epochs
+    epochs = 100 # The number of epochs
     best_model = None
     
     # TRAINING LOOP
@@ -327,10 +322,13 @@ if __name__ == '__main__':
         test_loss, math.exp(test_loss)))
     print('=' * 89)
     
+    savePATHduration = 'modelsDuration/modelDuration_'+ str(epochs) + 'epochs_padding.pt'
+    state_dictDuration = modelDuration.state_dict()
+    torch.save(state_dictDuration, savePATHduration)
+    
 
     #%% SAMPLES GENERATION
 
-    
     def getNote(val, dict_to_ix): 
         for key, value in dict_to_ix.items(): 
              if val == value: 
@@ -347,20 +345,187 @@ if __name__ == '__main__':
             melody4gen.append(getNote(word_index, dict_to_ix))
         return melody4gen
     
+    #specify the path
+    f='data/w_jazz/CharlieParker_DonnaLee_FINAL.mid'
+    melody4gen_pitch = (read_midi_pitch(f))[:80]
+    melody4gen_duration = (read_midi_duration(f))[:80]
     
-    #notes2gen = 40 # number of new notes to generate
-    #row_of_test = 0
-    #melody4gen_pitch = test_pitch[row_of_test][:80] # pitch of the input for generation
-    #melody4gen_duration = test_duration[row_of_test][:80] # duration of the input for generation
-    #new_melody_pitch = generate(modelPitch, melody4gen_pitch, pitch_to_ix, next_notes=notes2gen)
-    #new_melody_duration = generate(modelDuration, melody4gen_duration, duration_to_ix, notes2gen)
+    notes2gen = 40 # number of new notes to generate
+    new_melody_pitch = generate(modelPitch, melody4gen_pitch, pitch_to_ix, next_notes=notes2gen)
+    new_melody_duration = generate(modelDuration, melody4gen_duration, duration_to_ix, notes2gen)
 
-
-    #print(melody4gen_pitch)
+    midi_gen = convert_to_midi(new_melody_pitch, new_melody_duration)
+    midi_gen.write('midi', fp='output/gen4eval/music.mid') 
     
-    #convert_to_midi(new_melody_pitch, new_melody_duration)
-
     
+    # SAVE MODELS
+    
+    savePATHpitch = 'modelsPitch/modelPitch_'+ str(epochs) + 'epochs_padding.pt'
+    state_dictPitch = modelPitch.state_dict()
+    torch.save(state_dictPitch, savePATHpitch)
+
+    savePATHduration = 'modelsDuration/modelDuration_'+ str(epochs) + 'epochs_padding.pt'
+    state_dictDuration = modelDuration.state_dict()
+    torch.save(state_dictDuration, savePATHduration)
+    
+    
+    #%% MODEL EVALUATION
+    # accuracy, perplexity (paper seq-Attn)
+    # NLL loss, BLEU, MGeval (paper explicitly conditioned melody generation)
+    
+    def bleu_score():
+        pass
+    
+    def perplexity():
+        pass
+    
+    def accuracy_score(y_true, y_pred):
+        y_pred = np.concatenate(tuple(y_pred))
+        y_true = np.concatenate(tuple([[t for t in y] for y in y_true])).reshape(y_pred.shape)
+        return (y_true == y_pred).sum() / float(len(y_true))
+    
+    def evaluateAll(test_data, model, dict_to_ix):
+        eval_metrics = []
+        y_true = []
+        y_pred = []
+        for seq in test_data:
+            # Not doable: change approach !!!
+            for i in range(1000, len(seq)-1):
+                seq4pred = np.array(seq[:i])
+                target = seq[i+1]
+                target_hat = generate(model, seq4pred, dict_to_ix)[-1]
+                y_true.append(target)
+                y_pred.append(target_hat)
+        accuracy = accuracy_score(y_true, y_pred)
+        eval_metrics['accuracy'] = accuracy
+        return eval_metrics
+
+    #evaluateModelPitch = evaluateAll(test_pitch[0], modelPitch, pitch_to_ix)
+    
+    #%% MGEval 
+    
+    
+    def MGEval(training_midi_path, generated_midi_path, num_samples = 20):
+        
+        # Build a dataset of generated sequences (coherent with training data)
+        # How to build the dataset of generated sequences? 
+        # How many tokens for each sequence? 
+        # With how many samples to compare?
+        # ---------
+        # Calculate MGEval metrics for the two datasets and compute KL-divergence
+        
+        import glob
+        import seaborn as sns
+        import matplotlib.pyplot as plt
+        from mgeval import core, utils
+        from sklearn.model_selection import LeaveOneOut
+        
+        # Initialize dataset1 (training data)
+        set1 = glob.glob(training_midi_path)
+        # Dictionary of metrics
+        set1_eval = {'total_used_pitch':np.zeros((num_samples,1))}
+        # Add metrics to the dictionary
+        set1_eval['total_pitch_class_histogram'] = np.zeros((num_samples,12))
+        set1_eval['total_used_note'] = np.zeros((num_samples,1))
+        set1_eval['pitch_class_transition_matrix'] = np.zeros((num_samples,12,12))
+        set1_eval['pitch_range'] = np.zeros((num_samples,1))
+        #set1_eval['avg_pitch_shift'] = np.zeros((num_samples,1))
+        set1_eval['avg_IOI'] = np.zeros((num_samples,1))
+        set1_eval['note_length_hist'] = np.zeros((num_samples,12))
+        
+        # Calculate metrics
+        metrics_list = list(set1_eval.keys())
+        for j in range(0, len(metrics_list)):
+            for i in range(0, num_samples):
+                feature = core.extract_feature(set1[i])
+                set1_eval[metrics_list[j]][i] = getattr(core.metrics(), metrics_list[j])(feature)
+        
+        
+        # Initialize dataset2 (generated samples)
+        set2 = glob.glob(generated_midi_path)
+        # Dictionary of metrics
+        set2_eval = {'total_used_pitch':np.zeros((num_samples,1))}
+        # Add metrics to the dictionary
+        set2_eval['total_pitch_class_histogram'] = np.zeros((num_samples,12))
+        set2_eval['total_used_note'] = np.zeros((num_samples,1))
+        set2_eval['pitch_class_transition_matrix'] = np.zeros((num_samples,12,12))
+        set2_eval['pitch_range'] = np.zeros((num_samples,1))
+        #set2_eval['avg_pitch_shift'] = np.zeros((num_samples,1))
+        set2_eval['avg_IOI'] = np.zeros((num_samples,1))
+        set2_eval['note_length_hist'] = np.zeros((num_samples,12))
+        
+        # Calculate metrics
+        for j in range(0, len(metrics_list)):
+            for i in range(0, num_samples):
+                feature = core.extract_feature(set2[i])
+                set2_eval[metrics_list[j]][i] = getattr(core.metrics(), metrics_list[j])(feature)
+            
+        # Print the results
+        for i in range(0, len(metrics_list)):
+            print( metrics_list[i] + ':')
+            print('------------------------')
+            print(' demo_set')
+            print('  mean: ', np.mean(set1_eval[metrics_list[i]], axis=0))
+            print('  std: ', np.std(set1_eval[metrics_list[i]], axis=0))
+        
+            print('------------------------')
+            print(' demo_set')
+            print('  mean: ', np.mean(set2_eval[metrics_list[i]], axis=0))
+            print('  std: ', np.std(set2_eval[metrics_list[i]], axis=0))
+            
+            print()
+        
+        # exhaustive cross-validation for intra-set distances measurement
+        loo = LeaveOneOut()
+        loo.get_n_splits(np.arange(num_samples))
+        set1_intra = np.zeros((num_samples, len(metrics_list), num_samples-1))
+        set2_intra = np.zeros((num_samples, len(metrics_list), num_samples-1))
+        j = 0
+        for metric in metrics_list:
+            for train_index, test_index in loo.split(np.arange(num_samples)):
+                set1_intra[test_index[0]][j] = utils.c_dist(set1_eval[metric][test_index], set1_eval[metric][train_index])
+                set2_intra[test_index[0]][j] = utils.c_dist(set2_eval[metric][test_index], set2_eval[metric][train_index])
+            j += 1
+        
+        
+        # exhaustive cross-validation for inter-set distances measurement
+        loo = LeaveOneOut()
+        loo.get_n_splits(np.arange(num_samples))
+        sets_inter = np.zeros((num_samples, len(metrics_list), num_samples))
+        j = 0
+        for metric in metrics_list:
+            for train_index, test_index in loo.split(np.arange(num_samples)):
+                sets_inter[test_index[0]][j] = utils.c_dist(set1_eval[metric][test_index], set2_eval[metric])
+            j += 1
+        
+        # Plotting the results
+        plot_set1_intra = np.transpose(set1_intra,(1, 0, 2)).reshape(len(metrics_list), -1)
+        plot_set2_intra = np.transpose(set2_intra,(1, 0, 2)).reshape(len(metrics_list), -1)
+        plot_sets_inter = np.transpose(sets_inter,(1, 0, 2)).reshape(len(metrics_list), -1)
+        for i in range(0,len(metrics_list)):
+            sns.kdeplot(plot_set1_intra[i], label='intra_set1')
+            sns.kdeplot(plot_sets_inter[i], label='inter')
+            sns.kdeplot(plot_set2_intra[i], label='intra_set2')
+        
+            plt.title(metrics_list[i])
+            plt.xlabel('Euclidean distance')
+            plt.show()
+            
+        # Calculate divergence between measures
+        for i in range(0, len(metrics_list)):
+            print(metrics_list[i] + ':')
+            print('------------------------')
+            print(' demo_set1')
+            print('  Kullback–Leibler divergence:',utils.kl_dist(plot_set1_intra[i], plot_sets_inter[i]))
+            print('  Overlap area:', utils.overlap_area(plot_set1_intra[i], plot_sets_inter[i]))
+            
+            print(' demo_set2')
+            print('  Kullback–Leibler divergence:',utils.kl_dist(plot_set2_intra[i], plot_sets_inter[i]))
+            print('  Overlap area:', utils.overlap_area(plot_set2_intra[i], plot_sets_inter[i]))
+            print()
+     
+
+    # Check if it works next time and if it does delete this
     # Convert the output of the model to midi
     def convert_to_midi(new_melody_pitch, new_melody_duration):
         offset = 0
@@ -368,50 +533,69 @@ if __name__ == '__main__':
         #output_notes = stream.Stream()
         # create note and chord objects based on the values generated by the model
         for i in range(len(new_melody_pitch)):
-            if new_melody_pitch[i] == 'R':
+            if new_melody_pitch[i] == 'R' and new_melody_pitch[i] != '<sos>' and new_melody_pitch[i] != '<eos>' and new_melody_duration[i] != '<sos>' and new_melody_duration[i] != '<eos>':
                 new_note = note.Rest()
                 new_note.offset = offset
                 #new_note.duration.quarterLength = float(new_melody_duration[i])
-            else: 
+            elif new_melody_pitch[i] != '<sos>' and new_melody_pitch[i] != '<eos>' and new_melody_duration[i] != '<sos>' and new_melody_duration[i] != '<eos>': 
                 new_note = note.Note()
                 new_note.pitch.nameWithOctave = new_melody_pitch[i]
                 new_note.offset = offset
                 #new_note.duration.quarterLength = float(new_melody_duration[i])
                 new_note.storedInstrument = instrument.Piano()
             output_notes.append(new_note)
-            offset += float(new_melody_duration[i])
+            if new_melody_duration[i] != '<sos>' and new_melody_duration[i] != '<eos>': 
+                offset += float(new_melody_duration[i])
             
         midi_stream = stream.Stream(output_notes)
-        midi_stream.write('midi', fp='output/music.mid')  
+        return midi_stream
+        
+    # Remove characters who are not in the dictionary
+    def onlyDict(array, vocab):
+        # takes an array and a dictionary and gives the same array without
+        # the elements who are not in the dictionary
+        new_array=[]
+        for note in array:
+            if note in vocab:
+                new_array.append(note)            
+        new_array = np.array(new_array) # same solos but with only most frequent notes
+        return new_array
+    
+    
+    training_path = 'data/w_jazz_mini/*.mid'
+    
+    # Build a dataset of generated samples
+    import glob
+    standards = glob.glob(training_path)
+    num_of_generations = 10
+    j=0
+    for i in range(0, num_of_generations):
+        
+        print(standards[i])
+        melody4gen_pitch = (read_midi_pitch(standards[i]))[:40]
+        melody4gen_pitch = onlyDict(melody4gen_pitch, vocabPitch)
+        melody4gen_duration = (read_midi_duration(standards[i]))[:40]
+        melody4gen_duration = onlyDict(melody4gen_duration, vocabDuration)
+        
+        # in case one sequence has less elements than another one
+        if len(melody4gen_pitch) > len(melody4gen_duration):
+            diff = len(melody4gen_pitch) - len(melody4gen_duration)
+            melody4gen_pitch = melody4gen_pitch[:diff]
+        elif len(melody4gen_pitch) < len(melody4gen_duration):
+            diff = len(melody4gen_duration) - len(melody4gen_pitch)
+            melody4gen_pitch = melody4gen_duration[:diff]
+        
+        notes2gen = 20 # number of new notes to generate
+        new_melody_pitch = generate(modelPitch, melody4gen_pitch, pitch_to_ix, next_notes=notes2gen)
+        new_melody_duration = generate(modelDuration, melody4gen_duration, duration_to_ix, notes2gen)
+    
+        midi_gen = convert_to_midi(new_melody_pitch, new_melody_duration)
+        midi_gen.write('midi', fp='output/gen4eval/music'+str(j)+'.mid') 
+        j+=1
+        
+    generated_path = 'output/gen4eval/*.mid'
 
-    
-    #specify the path
-    f='data/w_jazz/ArtPepper_BluesForBlanche_FINAL.mid'
-    melody4gen_pitch = (read_midi_pitch(f))[:80]
-    melody4gen_duration = (read_midi_duration(f))[:80]
-    print(melody4gen_pitch)
-    print(melody4gen_duration)
-    
-    notes2gen = 40 # number of new notes to generate
-    new_melody_pitch = generate(modelPitch, melody4gen_pitch, pitch_to_ix, next_notes=notes2gen)
-    new_melody_duration = generate(modelDuration, melody4gen_duration, duration_to_ix, notes2gen)
-
-    convert_to_midi(new_melody_pitch, new_melody_duration)
-
-    #convert_to_midi(melody4gen_pitch, melody4gen_duration)
-    
-    
-    # SAVE MODELS
-    
-    savePATH = 'modelsPitch/modelPitch.pt'
-    torch.save(modelPitch, savePATH)
-    savePATH = 'modelsDuration/modelDuration.pt'
-    torch.save(modelDuration, savePATH)
-    
-    
-    #%% MODEL EVALUATION
-    # accuracy, perplexity (paper seq-Attn)
-    # BLEU, MGeval (paper explicitly conditioned melody generation)
+    MGEval(training_path, generated_path, num_of_generations)
     
     
     
