f45d3bec46d53b5358e31e72eac12b390b64069e
Vincenzo Madaghiele <vincenzo.madaghiele@gmail.com>
2020-11-08 00:19:04 +0100

    converted to pretty_midi

--- dataset_funct.py
+++ dataset_funct.py
@@ -7,102 +7,11 @@ Created on Mon Oct 26 11:44:03 2020
 """
 
 # library for understanding music
-from music21 import *
+#from music21 import *
 from torch.utils.data import Dataset
 import numpy as np
+import pretty_midi
 
-# define the Duration Dataset object for Pytorch
-class ImprovDurationDataset(Dataset):
-    
-    """
-    --> DataLoader can do the batch computation for us
-
-    Implement a custom Dataset:
-    inherit Dataset
-    implement __init__ , __getitem__ , and __len__
-    """
-    
-    def __init__(self):
-        #for listing down the file names
-        import os
-        
-        #specify the path
-        path='data/w_jazz/'
-        #read all the filenames
-        files=[i for i in os.listdir(path) if i.endswith(".mid")]
-        #reading each midi file
-        notes_array = np.array([read_midi_duration(path+i) for i in files])
-        
-        #converting 2D array into 1D array
-        notes_ = [element for note_ in notes_array for element in note_]
-        #No. of unique notes
-        unique_notes = list(set(notes_))
-        print("number of unique durations: " + str(len(unique_notes)))
-        
-        from collections import Counter
-        #computing frequency of each note
-        freq = dict(Counter(notes_))
-              
-        # the threshold for frequent notes can change 
-        threshold = 10 
-        frequent_notes = [note_ for note_, count in freq.items() if count>=threshold]
-        print("number of frequent durations (more than 10 times): " + str(len(frequent_notes)))
-        self.num_frequent_notes = len(frequent_notes)
-        self.vocab = frequent_notes
-        
-        # prepare new musical files which contain only the top frequent notes
-        new_music=[]
-        for notes in notes_array:
-            temp=[]
-            for note_ in notes:
-                if note_ in frequent_notes:
-                    temp.append(note_)            
-            new_music.append(temp)
-        new_music = np.array(new_music) # same solos but with only most frequent notes
-
-        self.x = new_music
-
-    # support indexing such that dataset[i] can be used to get i-th sample
-    def __getitem__(self, index):
-        return self.x_data[index], self.y_data[index]
-
-    # we can call len(dataset) to return the size
-    def __len__(self):
-        return self.n_samples
-    
-    def getData(self):
-        return self.x
-
-#defining function to read MIDI files duration
-def read_midi_duration(file):
-    
-    print("Loading Music File:",file)
-    
-    notes=[]
-    notes_to_parse = None
-    
-    #parsing a midi file
-    midi = converter.parse(file)
-    
-    #grouping based on different instruments
-    s2 = instrument.partitionByInstrument(midi)
-
-    #Looping over all the instruments
-    for part in s2.parts:
-        notes_to_parse = part.recurse() 
-        #finding whether a particular element is note or a chord
-        for element in notes_to_parse:        
-            #note
-            if isinstance(element, note.Note):
-                # Read only duration of the note
-                notes.append((element.duration.quarterLength))
-            elif isinstance(element, note.Rest):
-                notes.append((element.duration.quarterLength))
-            #chord
-            #elif isinstance(element, chord.Chord):
-                #notes.append('.'.join(str(n) for n in element.normalOrder))
-                
-    return np.array(notes)
 
 # define the Pitch Dataset object for Pytorch
 class ImprovPitchDataset(Dataset):
@@ -120,11 +29,11 @@ class ImprovPitchDataset(Dataset):
         import os
         
         #specify the path
-        path='data/w_jazz/'
+        path='data/w_jazz_augmented/'
         #read all the filenames
         files=[i for i in os.listdir(path) if i.endswith(".mid")]
         #reading each midi file
-        notes_array = np.array([read_midi_pitch(path+i) for i in files])
+        notes_array = np.array([np.array(readMIDI(path+i)[0]) for i in files])
         
         #converting 2D array into 1D array
         notes_ = [element for note_ in notes_array for element in note_]
@@ -166,56 +75,215 @@ class ImprovPitchDataset(Dataset):
     def getData(self):
         return self.x
 
-#defining function to read MIDI files
-def read_midi_pitch(file):
+
+# define the Duration Dataset object for Pytorch
+class ImprovDurationDataset(Dataset):
     
+    """
+    --> DataLoader can do the batch computation for us
+
+    Implement a custom Dataset:
+    inherit Dataset
+    implement __init__ , __getitem__ , and __len__
+    """
+    
+    def __init__(self):
+        #for listing down the file names
+        import os
+        
+        #specify the path
+        path='data/w_jazz_augmented/'
+        #read all the filenames
+        files=[i for i in os.listdir(path) if i.endswith(".mid")]
+        #reading each midi file
+        notes_array = np.array([np.array(readMIDI(path+i)[1]) for i in files])
+        
+        #converting 2D array into 1D array
+        notes_ = [element for note_ in notes_array for element in note_]
+        #No. of unique notes
+        unique_notes = list(set(notes_))
+        print("number of unique durations: " + str(len(unique_notes)))
+        
+        from collections import Counter
+        #computing frequency of each note
+        freq = dict(Counter(notes_))
+              
+        # the threshold for frequent notes can change 
+        threshold = 10 
+        frequent_notes = [note_ for note_, count in freq.items() if count>=threshold]
+        print("number of frequent durations (more than 10 times): " + str(len(frequent_notes)))
+        self.num_frequent_notes = len(frequent_notes)
+        self.vocab = frequent_notes
+        
+        # prepare new musical files which contain only the top frequent notes
+        new_music=[]
+        for notes in notes_array:
+            temp=[]
+            for note_ in notes:
+                if note_ in frequent_notes:
+                    temp.append(note_)            
+            new_music.append(temp)
+        new_music = np.array(new_music) # same solos but with only most frequent notes
+
+        self.x = new_music
+
+    # support indexing such that dataset[i] can be used to get i-th sample
+    def __getitem__(self, index):
+        return self.x_data[index], self.y_data[index]
+
+    # we can call len(dataset) to return the size
+    def __len__(self):
+        return self.n_samples
+    
+    def getData(self):
+        return self.x
+
+
+def readMIDI(file):
+    '''
+    
+    Parameters
+    ----------
+    file : path to a midi file (string)
+        the midi file is read by the function.
+
+    Returns
+    -------
+    notes : list of notes in midi number format
+        this list contains all of the notes and the rests in the song.
+        The duration of each note is in the correspondent index of the durations array
+    durations : list of durations in musical terms (string)
+        this list contains the durations of each note and rest in the song.
+        The pitch of each note is in the correspondent index of the durations array
+    dur_dict : python dictionary
+        the keys of this dictionary are the time in seconds 
+        of each possible note/rest duration for this song.
+    song_properties : python dictionary
+        this dictionary contains some basic properties of the midi file.
+
+    '''
+
+    pm = pretty_midi.PrettyMIDI(file)
     print("Loading Music File:",file)
     
-    notes=[]
-    notes_to_parse = None
+    # Get and downbeat times
+    beats = pm.get_beats()
+    downbeats = pm.get_downbeats()
     
-    #parsing a midi file
-    midi = converter.parse(file)
+    song_properties = {}
+    song_properties['tempo'] = pm.estimate_tempo()
+    song_properties['beat duration'] = beats[1] - beats[0]
+    song_properties['measure duration'] = downbeats[2] - downbeats[0]
     
-    #grouping based on different instruments
-    s2 = instrument.partitionByInstrument(midi)
-
-    #Looping over all the instruments
-    for part in s2.parts:
-        notes_to_parse = part.recurse() 
-        #finding whether a particular element is note or a rest
-        for element in notes_to_parse:        
-            #note
-            if isinstance(element, note.Note):
-                notes.append(str(element.pitch))
-            elif isinstance(element, note.Rest):
-                notes.append('R')
-            #chord
-            #elif isinstance(element, chord.Chord):
-                #notes.append('.'.join(str(n) for n in element.normalOrder))
-                
-    return np.array(notes)
-
-# Convert the output of the model to midi
-def convert_to_midi(new_melody_pitch, new_melody_duration):
-    offset = 0
-    output_notes = []
-    #output_notes = stream.Stream()
-    # create note and chord objects based on the values generated by the model
-    for i in range(len(new_melody_pitch)):
-        if new_melody_pitch[i] == 'R' and new_melody_pitch[i] != '<sos>' and new_melody_pitch[i] != '<eos>' and new_melody_duration[i] != '<sos>' and new_melody_duration[i] != '<eos>':
-            new_note = note.Rest()
-            new_note.offset = offset
-            #new_note.duration.quarterLength = float(new_melody_duration[i])
-        elif new_melody_pitch[i] != '<sos>' and new_melody_pitch[i] != '<eos>' and new_melody_duration[i] != '<sos>' and new_melody_duration[i] != '<eos>': 
-            new_note = note.Note()
-            new_note.pitch.nameWithOctave = new_melody_pitch[i]
-            new_note.offset = offset
-            #new_note.duration.quarterLength = float(new_melody_duration[i])
-            new_note.storedInstrument = instrument.Piano()
-        output_notes.append(new_note)
-        if new_melody_duration[i] != '<sos>' and new_melody_duration[i] != '<eos>': 
-            offset += float(new_melody_duration[i])
+    # sampling of the measure
+    unit = song_properties['measure duration'] / 96.
+    # possible note durations in seconds 
+    # (it is possible to add representations - include 32nds, quintuplets...):
+    # [full, half, quarter, 8th, 16th, dot half, dot quarter, dot 8th, dot 16th, half note triplet, quarter note triplet, 8th note triplet]
+    possible_durations = [unit * 96, unit * 48, unit * 24, unit * 12, unit * 6, unit * 72, 
+                          unit * 36, unit * 18, unit * 9, unit * 32, unit * 16, unit * 8]
+    
+    # Define durations dictionary
+    dur_dict = {}
+    dur_dict[possible_durations[0]] = 'full'
+    dur_dict[possible_durations[1]] = 'half'
+    dur_dict[possible_durations[2]] = 'quarter'
+    dur_dict[possible_durations[3]] = '8th'
+    dur_dict[possible_durations[4]] = '16th'
+    dur_dict[possible_durations[5]] = 'dot half'
+    dur_dict[possible_durations[6]] = 'dot quarter'
+    dur_dict[possible_durations[7]] = 'dot 8th'
+    dur_dict[possible_durations[8]] = 'dot 16th'
+    dur_dict[possible_durations[9]] = 'half note triplet'
+    dur_dict[possible_durations[10]] = 'quarter note triplet'
+    dur_dict[possible_durations[11]] = '8th note triplet'
+    
+    # compile the lists of pitchs and durations
+    notes = []
+    durations = []
+    for instrument in range(len(pm.instruments)):
+        for note in range(len(pm.instruments[instrument].notes)-1):
+            # append pitch
+            notes.append(str(pm.instruments[instrument].notes[note].pitch))
+            # calculate note duration in secods
+            duration_sec = pm.instruments[instrument].notes[note].end - pm.instruments[instrument].notes[note].start
+            # calculate distance from each duration
+            distance = np.abs(np.array(possible_durations) - duration_sec)
+            idx = distance.argmin()
+            durations.append(dur_dict[possible_durations[idx]])
             
-    midi_stream = stream.Stream(output_notes)
-    return midi_stream
+            # check for rests
+            intra_note_time = pm.instruments[instrument].notes[note+1].start - pm.instruments[instrument].notes[note].end
+            # if the interval between notes is greater than the smallest duration ('16th')
+            # and smaller than the greatest duration ('full') then there is a rest
+            if intra_note_time >= possible_durations[4]:
+                # there is a rest!
+                
+                # handle the possibility of pauses longer than a full note
+                while intra_note_time > possible_durations[0]:
+                    notes.append('R')
+                    # calculate distance from each duration
+                    distance = np.abs(np.array(possible_durations) - intra_note_time)
+                    idx = distance.argmin()
+                    durations.append(dur_dict[possible_durations[idx]])
+                    intra_note_time -= possible_durations[idx]
+                
+                notes.append('R')
+                # calculate distance from each duration
+                distance = np.abs(np.array(possible_durations) - intra_note_time)
+                idx = distance.argmin()
+                durations.append(dur_dict[possible_durations[idx]])
+    return notes, durations, dur_dict, song_properties
+
+def getKey(val, dict_to_ix): 
+    for key, value in dict_to_ix.items(): 
+        if val == value: 
+            return key
+
+def convertMIDI(notes, durations, tempo, dur_dict):
+    '''
+    
+    Parameters
+    ----------
+    pitches : list of pitches
+        a list of all the pitches and the rests in the song to be exported.
+        Each pitch/rest should have its own duration 
+        at the same index of the durations list.
+    durations : list of durations
+        a list of all the durations of the pitches/rests in the song to be exported.
+        Each duration should have its own pitch/rest 
+        at the same index of the pitches list.
+    tempo : integer
+        tempo of the song.
+
+    Returns
+    -------
+    pm : pretty_midi object
+        this pretty midi can be exported to midi and saved.
+
+    '''
+    # pitches and durations must be of equal lenght
+    # still does not include rests
+    
+    # Construct a PrettyMIDI object.
+    pm = pretty_midi.PrettyMIDI(initial_tempo=tempo)
+    # Add a piano instrument
+    inst = pretty_midi.Instrument(program=1, is_drum=False, name='piano')
+    pm.instruments.append(inst)
+    # Let's add a few notes to our instrument
+    velocity = 100
+    offset = 0
+    # for each note
+    for i in range(len(notes)):
+        if notes[i] != '<pad>' and notes[i] != '<sos>' and notes[i] != '<eos>' and durations[i] != '<pad>' and durations[i] != '<sos>' and durations[i] != '<eos>':
+            if notes[i] == 'R':
+                duration = getKey(durations[i],dur_dict)
+            else:
+                pitch = int(notes[i])
+                duration = getKey(durations[i],dur_dict)
+                start = offset
+                end = offset + duration
+                inst.notes.append(pretty_midi.Note(velocity, pitch, start, end))
+            offset += duration
+    return pm
+
