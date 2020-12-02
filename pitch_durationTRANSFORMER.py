241497f29e5ea8ceb0f25254075b99a95277c51b
Vincenzo Madaghiele <vincenzo.madaghiele@gmail.com>
2020-12-02 08:51:24 +0100

    added: segmentation

--- pitch_durationTRANSFORMER.py
+++ pitch_durationTRANSFORMER.py
@@ -410,7 +410,7 @@ if __name__ == '__main__':
     
     # TRAIN AND EVALUATE LOSS
     best_val_loss = float("inf")
-    epochs = 100 # The number of epochs
+    epochs = 10 # The number of epochs
     best_model = None
 
     
@@ -465,7 +465,7 @@ if __name__ == '__main__':
     
     # TRAIN AND EVALUATE LOSS
     best_val_loss = float("inf")
-    epochs = 100 # The number of epochs
+    epochs = 10 # The number of epochs
     best_model = None
     
     # TRAINING LOOP
