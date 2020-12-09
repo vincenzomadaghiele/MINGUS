325c770b8f86b3fb6eda6bf12b605f166ea7eafb
Vincenzo Madaghiele <vincenzo.madaghiele@gmail.com>
2020-12-09 10:46:21 +0100

    fixed: transformer and encoder now working

--- MINGUS_dataset_funct.py
+++ MINGUS_dataset_funct.py
@@ -370,11 +370,11 @@ def batchify(data, bsz, dict_to_ix, device):
 
         
     # Divide the dataset into bsz parts.
-    #nbatch = data.size(0) // bsz
+    nbatch = data.size(0) // bsz
     # Trim off any extra elements that wouldn't cleanly fit (remainders).
-    #data = data.narrow(0, 0, nbatch * bsz)
+    data = data.narrow(0, 0, nbatch * bsz)
     # Evenly divide the data across the bsz batches.
-    #data = data.view(bsz, -1).t().contiguous()
+    data = data.view(bsz, -1).t().contiguous()
         
     return data.to(device)
 
