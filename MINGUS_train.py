025f63c47692ec145c7dcb00d9e318a4b7714e3b
Vincenzo Madaghiele <vincenzo.madaghiele@gmail.com>
2020-12-10 09:07:27 +0100

    order in the folders

--- MINGUS_train.py
+++ MINGUS_train.py
@@ -1,238 +1,48 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Tue Dec  8 09:56:44 2020
+Created on Mon Oct 26 11:47:05 2020
 
 @author: vincenzomadaghiele
+
+This scripts recreates the 'no_cond' model of the paper:
+    Explicitly Conditioned Melody Generation: A Case Study with Interdependent RNNs
+    (Genchel et al., 2019)
+but using Transformers instead of RNNs
+and with a different dataset
+
+This code is used for training and generation of samples
+
+Things to do:
+    - implement remaining metrics (BLEU, MGEval)
+    - run model with other datasets (seAttn data) and compare metrics
+    - make net scheme
+    - grid search for model optimization
+    - conditioning on chords and inter-conditioning between pitch and duration
+    - move all constants to an external .py file
 """
+
+import pretty_midi
 import torch
 import torch.nn as nn
-import torch.optim as optim
-
 import numpy as np
 import math
-import time
-import pretty_midi
-
-import MINGUS_dataset_funct as dt
+#from MINGUS_dataset_funct import ImprovDurationDataset, ImprovPitchDataset, readMIDI, convertMIDI
+import MINGUS_dataset_funct as dataset
+import MINGUS_model as mod
 
 # Device configuration
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 torch.manual_seed(1)
 
 
-# TRANSFORMER MODEL
-class Transformer(nn.Module):
-
-    def __init__(self, 
-                 ntoken, 
-                 ninp, 
-                 nhead, 
-                 n_enc, 
-                 n_dec, 
-                 forward_expansion, 
-                 src_pad_idx, 
-                 device, 
-                 dropout=0.5):
-        super(Transformer, self).__init__()
-        #from torch.nn import TransformerEncoder, TransformerEncoderLayer
-        self.model_type = 'Transformer'
-        self.src_mask = None
-        self.src_pad_idx = src_pad_idx
-        self.device = device
-        self.ninp = ninp
-        
-        self.src_embedding = nn.Embedding(ntoken, ninp)
-        self.src_pos_encoder = PositionalEncoding(ninp, dropout)
-        self.trg_embedding = nn.Embedding(ntoken, ninp)
-        self.trg_pos_encoder = PositionalEncoding(ninp, dropout)
-        
-        self.transformer = nn.Transformer(
-            ninp,
-            nhead,
-            n_enc,
-            n_dec,
-            forward_expansion,
-            dropout,
-        )
-        
-        self.fc_out = nn.Linear(ninp, ntoken)
-        #self.init_weights()
-
-    def generate_square_subsequent_mask(self, sz):
-        mask = (torch.triu(torch.ones(sz, sz)) == 1).transpose(0, 1)
-        mask = mask.float().masked_fill(mask == 0, float('-inf')).masked_fill(mask == 1, float(0.0))
-        return mask
-
-    def make_pad_mask(self, src):
-        pad_mask = src.transpose(0, 1) == self.src_pad_idx
-        #pad_mask = pad_mask.float().masked_fill(pad_mask == True, float('-inf')).masked_fill(pad_mask == False, float(0.0))
-        return pad_mask.to(self.device)
-
-    def init_weights(self):
-        initrange = 0.1
-        self.transformer.weight.data.uniform_(-initrange, initrange)
-        self.transformer.bias.data.zero_()
-        self.transformer.weight.data.uniform_(-initrange, initrange)
-
-    def forward(self, src, trg, trg_mask):
-        src_padding_mask = self.make_pad_mask(src)
-        tgt_padding_mask = self.make_pad_mask(trg)
-        
-        src = self.src_embedding(src) * math.sqrt(self.ninp)
-        trg = self.trg_embedding(trg) * math.sqrt(self.ninp)
-        src = self.src_pos_encoder(src)
-        trg = self.trg_pos_encoder(trg)
-        
-        output = self.transformer(src, 
-                                  trg, 
-                                  tgt_mask=trg_mask, 
-                                  src_key_padding_mask=src_padding_mask, 
-                                  tgt_key_padding_mask=tgt_padding_mask)
-
-        output = self.fc_out(output)
-        return output
-
-# POSITIONAL ENCODING
-class PositionalEncoding(nn.Module):
-
-    def __init__(self, d_model, dropout=0.1, max_len=5000):
-        super(PositionalEncoding, self).__init__()
-        self.dropout = nn.Dropout(p=dropout)
-
-        pe = torch.zeros(max_len, d_model)
-        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
-        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
-        pe[:, 0::2] = torch.sin(position * div_term)
-        pe[:, 1::2] = torch.cos(position * div_term)
-        pe = pe.unsqueeze(0).transpose(0, 1)
-        self.register_buffer('pe', pe)
-
-    def forward(self, x):
-        x = x + self.pe[:x.size(0), :]
-        return self.dropout(x)
-
-def get_batch(source, i, bptt):
-    '''
-
-    Parameters
-    ----------
-    source : pytorch Tensor
-        source of batched data.
-    i : integer
-        number of the batch to be selected.
-
-    Returns
-    -------
-    data : pytorch Tensor
-        batch of data of size [bptt x bsz].
-    target : pytorch Tensor
-        same as data but sequences are shifted by one token, 
-        of size [bptt x bsz].
-
-    '''
-    seq_len = min(bptt, len(source) - 1 - i)
-    data = source[i:i+seq_len] # input 
-    target = source[i+1:i+1+seq_len]#.view(-1) # target (same as input but shifted by 1)
-
-    return data, target
-
-def train(model, vocab, train_data, criterion, optimizer, bptt, device, epoch, scheduler):
-    '''
-
-    Parameters
-    ----------
-    model : pytorch Model
-        Model to be trained.
-    vocab : python dictionary
-        dictionary of the tokens used in training.
-    train_data : pytorch Tensor
-        batched data to be used in training.
-    criterion : pytorch Criterion
-        criterion to be used for otimization.
-    optimizer : pytorch Optimizer
-        optimizer used in training.
-
-    Returns
-    -------
-    None.
-
-    '''
-    model.train() # Turn on the train mode
-    total_loss = 0.
-    start_time = time.time()
-    ntokens = len(vocab)
-    src_mask = model.generate_square_subsequent_mask(bptt).to(device)
-    for batch, i in enumerate(range(0, train_data.size(0) - 1, bptt)):
-        data, targets = get_batch(train_data, i, bptt)
-        optimizer.zero_grad()
-        
-        if targets.size(0) != bptt:
-            src_mask = model.generate_square_subsequent_mask(targets.size(0)).to(device)
-        
-        output = model(data, targets, src_mask) 
-        loss = criterion(output.view(-1, ntokens), targets.view(-1))
-        
-        loss.backward()
-        torch.nn.utils.clip_grad_norm_(model.parameters(), 0.5)
-        optimizer.step()
-
-        total_loss += loss.item()
-        log_interval = 200
-        if batch % log_interval == 0 and batch > 0:
-            cur_loss = total_loss / log_interval
-            elapsed = time.time() - start_time
-            print('| epoch {:3d} | {:5d}/{:5d} batches | '
-                  'lr {:02.2f} | ms/batch {:5.2f} | '
-                  'loss {:5.2f} | ppl {:8.2f}'.format(
-                    epoch, batch, len(train_data) // bptt, 
-                    #scheduler.get_last_lr()[0],
-                    3.54,
-                    elapsed * 1000 / log_interval,
-                    cur_loss, math.exp(cur_loss)))
-            total_loss = 0
-            start_time = time.time()
-
-def evaluate(eval_model, data_source, vocab, bptt, device, criterion):
-    '''
-
-    Parameters
-    ----------
-    eval_model : pytorch Model
-        Model, already trained to be evaluated.
-    data_source : pytorch Tensor
-        Evaluation dataset.
-    vocab : python dictionary
-        dictionary used for tokenization.
-
-    Returns
-    -------
-    float
-        total loss of the model on validation/test data.
-
-    '''
-    eval_model.eval() # Turn on the evaluation mode
-    total_loss = 0.
-    ntokens = len(vocab)
-    src_mask = eval_model.generate_square_subsequent_mask(bptt).to(device)
-    with torch.no_grad():
-        for i in range(0, data_source.size(0) - 1, bptt):
-            data, targets = get_batch(data_source, i, bptt)
-            if data.size(0) != bptt:
-                src_mask = eval_model.generate_square_subsequent_mask(data.size(0)).to(device)
-            output = eval_model(data, targets, src_mask) 
-            total_loss += len(data) * criterion(output.view(-1, ntokens), targets.view(-1)).item()
-    return total_loss / (len(data_source) - 1)
-
-
-#%%
-
 if __name__ == '__main__':
+
     # DATA LOADING
     
     # LOAD PITCH DATASET
     pitch_path = 'data/w_jazz/'
-    datasetPitch = dt.ImprovPitchDataset(pitch_path, 20)
+    datasetPitch = dataset.ImprovPitchDataset(pitch_path, 20)
     X_pitch = datasetPitch.getData()
     # set vocabulary for conversion
     vocabPitch = datasetPitch.vocab
@@ -250,7 +60,7 @@ if __name__ == '__main__':
     
     # LOAD DURATION DATASET
     duration_path = 'data/w_jazz/'
-    datasetDuration = dt.ImprovDurationDataset(duration_path, 10)
+    datasetDuration = dataset.ImprovDurationDataset(duration_path, 10)
     X_duration = datasetDuration.getData()
     # set vocabulary for conversion
     vocabDuration = datasetDuration.vocab
@@ -266,70 +76,69 @@ if __name__ == '__main__':
     val_duration = X_duration[int(len(X_duration)*0.7)+1:int(len(X_duration)*0.7)+1+int(len(X_duration)*0.1)]
     test_duration = X_duration[int(len(X_duration)*0.7)+1+int(len(X_duration)*0.1):]
     
-    #%% DATA PREPARATION
     
-    # Melody segmentation
-    segment_length = 35 # Maximum value of a sequence
-    train_pitch_segmented, train_duration_segmented = dt.segmentDataset(train_pitch, train_duration, segment_length)
-    val_pitch_segmented, val_duration_segmented = dt.segmentDataset(val_pitch, val_duration, segment_length)
-    test_pitch_segmented, test_duration_segmented = dt.segmentDataset(test_pitch, test_duration, segment_length)
+    #%% MELODY SEGMENTATION
+    
+    # Maximum value of a sequence
+    segment_length = 35
+    train_pitch_segmented, train_duration_segmented = dataset.segmentDataset(train_pitch, train_duration, segment_length)
+    val_pitch_segmented, val_duration_segmented = dataset.segmentDataset(val_pitch, val_duration, segment_length)
+    test_pitch_segmented, test_duration_segmented = dataset.segmentDataset(test_pitch, test_duration, segment_length)
 
     
+    #%% DATA PREPARATION
+
     batch_size = 20
     eval_batch_size = 10
     
-    # pad, batch and convert to tensor
-    train_data_pitch = dt.batchify(train_pitch_segmented, batch_size, pitch_to_ix, device)
-    val_data_pitch = dt.batchify(val_pitch_segmented, eval_batch_size, pitch_to_ix, device)
-    test_data_pitch = dt.batchify(test_pitch_segmented, eval_batch_size, pitch_to_ix, device)
+    train_data_pitch = dataset.batchify(train_pitch_segmented, batch_size, pitch_to_ix, device)
+    val_data_pitch = dataset.batchify(val_pitch_segmented, eval_batch_size, pitch_to_ix, device)
+    test_data_pitch = dataset.batchify(test_pitch_segmented, eval_batch_size, pitch_to_ix, device)
     
-    train_data_duration = dt.batchify(train_duration_segmented, batch_size, duration_to_ix, device)
-    val_data_duration = dt.batchify(val_duration_segmented, eval_batch_size, duration_to_ix, device)
-    test_data_duration = dt.batchify(test_duration_segmented, eval_batch_size, duration_to_ix, device)
+    train_data_duration = dataset.batchify(train_duration_segmented, batch_size, duration_to_ix, device)
+    val_data_duration = dataset.batchify(val_duration_segmented, eval_batch_size, duration_to_ix, device)
+    test_data_duration = dataset.batchify(test_duration_segmented, eval_batch_size, duration_to_ix, device)
     
+    # divide into target and input sequence of lenght bptt
+    # --> obtain matrices of size bptt x batch_size
     # a padded sequence is of length segment_value + 2 (sos and eos tokens)
     bptt = segment_length #+ 2 # lenght of a sequence of data (IMPROVEMENT HERE!!)
-
-
-    #%% PITCH MODEL
     
-    ntoken = len(vocabPitch)
-    ninp = 200 # embedding dimension
-    nhead = 4 # number of attention heads
-    n_enc = 6 # number of encoder layers
-    n_dec = 6 # number of decoder layers
-    forward_expansion = 4 
-    pad_idx = pitch_to_ix['<pad>']
-    device = device
-    dropout=0.5
     
-    modelPitch = Transformer(ntoken, ninp, nhead, n_enc, n_dec, forward_expansion,
-                            pad_idx, device, dropout).to(device)
+    #%% PITCH MODEL TRAINING
     
-    # Training hyperparameters
-    num_epochs = 10
-    learning_rate = 3e-4
-    batch_size = 32
+    
+    # HYPERPARAMETERS
+    ntokens_pitch = len(vocabPitch) # the size of vocabulary
+    emsize = 200 # embedding dimension
+    nhid = 200 # the dimension of the feedforward network model in nn.TransformerEncoder
+    nlayers = 2 # the number of nn.TransformerEncoderLayer in nn.TransformerEncoder
+    nhead = 2 # the number of heads in the multiheadattention models
+    dropout = 0.2 # the dropout value
+    src_pad_idx = pitch_to_ix['<pad>']
+    modelPitch = mod.TransformerModel(ntokens_pitch, emsize, nhead, nhid, nlayers, 
+                                  src_pad_idx, device, dropout).to(device)
+    
+    # LOSS FUNCTION
+    criterion = nn.CrossEntropyLoss(ignore_index=src_pad_idx)
+    lr = 5.0 # learning rate
+    optimizer = torch.optim.SGD(modelPitch.parameters(), lr=lr)
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 1.0, gamma=0.95)
+    
+    # TRAIN AND EVALUATE LOSS
     best_val_loss = float("inf")
-    
-    criterion = nn.CrossEntropyLoss(ignore_index=pad_idx)
-    #lr = 5.0 # learning rate
-    #optimizer = torch.optim.SGD(modelPitch.parameters(), lr=lr)
-    #scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 1.0, gamma=0.95)
-     
-    optimizer = optim.Adam(modelPitch.parameters(), lr=learning_rate)
-    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
-        optimizer, factor=0.1, patience=10, verbose=True
-    )
+    epochs = 10 # The number of epochs
+    best_model = None
+
     
     # TRAINING LOOP
-    for epoch in range(1, num_epochs + 1):
+    for epoch in range(1, epochs + 1):
         
         epoch_start_time = time.time()
-        train(modelPitch, vocabPitch, train_data_pitch, criterion, optimizer, 
-                  bptt, device, epoch, scheduler)
-        val_loss = evaluate(modelPitch, val_data_pitch, vocabPitch, 
-                                bptt, device, criterion)
+        mod.train(modelPitch, vocabPitch, train_data_pitch, criterion, optimizer, 
+              scheduler, epoch, bptt, device)
+        val_loss = mod.evaluate(modelPitch, val_data_pitch, vocabPitch, 
+                            criterion, bptt, device)
         print('-' * 89)
         print('| end of epoch {:3d} | time: {:5.2f}s | valid loss {:5.2f} | '
               'valid ppl {:8.2f}'.format(epoch, (time.time() - epoch_start_time),
@@ -344,57 +153,49 @@ if __name__ == '__main__':
     
     
     # TEST THE MODEL
-    test_loss = evaluate(best_model_pitch, test_data_pitch, vocabPitch,  
-                             bptt, device, criterion)
+    test_loss = mod.evaluate(best_model_pitch, test_data_pitch, vocabPitch, 
+                         criterion, bptt, device)
     print('=' * 89)
     print('| End of training | test loss {:5.2f} | test ppl {:8.2f}'.format(
         test_loss, math.exp(test_loss)))
     print('=' * 89)
     
-    savePATHpitch = 'modelsPitch/transformerPitch_'+ str(num_epochs) + 'epochs_wjazz_segmented.pt'
+    savePATHpitch = 'modelsPitch/modelPitch_'+ str(epochs) + 'epochs_wjazz_segmented.pt'
     state_dictPitch = best_model_pitch.state_dict()
     torch.save(state_dictPitch, savePATHpitch)
     
+
+    #%% DURATION MODEL TRAINING
     
-    #%% DURATION MODEL
+    # HYPERPARAMETERS
+    ntokens_duration = len(vocabDuration) # the size of vocabulary
+    emsize = 200 # embedding dimension
+    nhid = 200 # the dimension of the feedforward network model in nn.TransformerEncoder
+    nlayers = 2 # the number of nn.TransformerEncoderLayer in nn.TransformerEncoder
+    nhead = 2 # the number of heads in the multiheadattention models
+    dropout = 0.2 # the dropout value
+    src_pad_idx = duration_to_ix['<pad>']
+    modelDuration = mod.TransformerModel(ntokens_duration, emsize, nhead, nhid, 
+                                     nlayers, src_pad_idx, device, dropout).to(device)
     
-    ntoken = len(vocabDuration)
-    ninp = 200 # embedding dimension
-    nhead = 4 # number of attention heads
-    n_enc = 6 # number of encoder layers
-    n_dec = 6 # number of decoder layers
-    forward_expansion = 4 
-    pad_idx = pitch_to_ix['<pad>']
-    device = device
-    dropout=0.5
+    # LOSS FUNCTION
+    criterion = nn.CrossEntropyLoss(ignore_index=src_pad_idx)
+    lr = 5.0 # learning rate
+    optimizer = torch.optim.SGD(modelDuration.parameters(), lr=lr)
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 1.0, gamma=0.95)
     
-    modelDuration = Transformer(ntoken, ninp, nhead, n_enc, n_dec, forward_expansion,
-                            pad_idx, device, dropout).to(device)
-    
-    # Training hyperparameters
-    num_epochs = 10
-    learning_rate = 3e-4
-    batch_size = 32
+    # TRAIN AND EVALUATE LOSS
     best_val_loss = float("inf")
-    
-    criterion = nn.CrossEntropyLoss(ignore_index=pad_idx)
-    #lr = 5.0 # learning rate
-    #optimizer = torch.optim.SGD(modelPitch.parameters(), lr=lr)
-    #scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 1.0, gamma=0.95)
-    
-    optimizer = optim.Adam(modelDuration.parameters(), lr=learning_rate)
-    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
-        optimizer, factor=0.1, patience=10, verbose=True
-    )
+    epochs = 10 # The number of epochs
+    best_model = None
     
     # TRAINING LOOP
-    for epoch in range(1, num_epochs + 1):
-        
+    for epoch in range(1, epochs + 1):
         epoch_start_time = time.time()
-        train(modelDuration, vocabDuration, train_data_duration, criterion, optimizer, 
-                  bptt, device, epoch, scheduler)
-        val_loss = evaluate(modelDuration, val_data_duration, vocabDuration, 
-                                bptt, device, criterion)
+        mod.train(modelDuration, vocabDuration, train_data_duration, criterion, optimizer, 
+              scheduler, epoch, bptt, device)
+        val_loss = mod.evaluate(modelDuration, val_data_duration, vocabDuration, 
+                            criterion, bptt, device)
         print('-' * 89)
         print('| end of epoch {:3d} | time: {:5.2f}s | valid loss {:5.2f} | '
               'valid ppl {:8.2f}'.format(epoch, (time.time() - epoch_start_time),
@@ -403,21 +204,20 @@ if __name__ == '__main__':
     
         if val_loss < best_val_loss:
             best_val_loss = val_loss
-            best_model_pitch = modelDuration
+            best_model_duration = modelDuration
     
         scheduler.step()
     
-    
     # TEST THE MODEL
-    test_loss = evaluate(modelDuration, val_data_duration, vocabDuration,  
-                             bptt, device, criterion)
+    test_loss = mod.evaluate(best_model_duration, test_data_duration, vocabDuration, 
+                         criterion, bptt, device)
     print('=' * 89)
     print('| End of training | test loss {:5.2f} | test ppl {:8.2f}'.format(
         test_loss, math.exp(test_loss)))
     print('=' * 89)
     
-    savePATHpitch = 'modelsPitch/transformerDuration_'+ str(num_epochs) + 'epochs_wjazz_segmented.pt'
-    state_dictPitch = best_model_pitch.state_dict()
-    torch.save(state_dictPitch, savePATHpitch)
-        
+    savePATHduration = 'modelsDuration/modelDuration_'+ str(epochs) + 'epochs_wjazz_segmented.pt'
+    state_dictDuration = best_model_duration.state_dict()
+    torch.save(state_dictDuration, savePATHduration)
+    
     
--- complete_transformer/MINGUS_model_decoderEncoder.py
+++ complete_transformer/MINGUS_model_decoderEncoder.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Thu Dec 10 08:55:01 2020
+Created on Tue Dec  8 10:26:31 2020
 
 @author: vincenzomadaghiele
 """
@@ -11,26 +11,45 @@ import torch.nn as nn
 import numpy as np
 import math
 import time
-import MINGUS_dataset_funct as dataset
+import MINGUS_dataset_funct as dt
 
 
 # TRANSFORMER MODEL
-class TransformerModel(nn.Module):
+class Transformer(nn.Module):
 
-    def __init__(self, ntoken, ninp, nhead, nhid, nlayers, src_pad_idx, device, dropout=0.5):
-        super(TransformerModel, self).__init__()
-        from torch.nn import TransformerEncoder, TransformerEncoderLayer
+    def __init__(self, 
+                 ntoken, 
+                 ninp, 
+                 nhead, 
+                 n_enc, 
+                 n_dec, 
+                 forward_expansion, 
+                 src_pad_idx, 
+                 device, 
+                 dropout=0.5):
+        super(Transformer, self).__init__()
+        #from torch.nn import TransformerEncoder, TransformerEncoderLayer
         self.model_type = 'Transformer'
         self.src_mask = None
         self.src_pad_idx = src_pad_idx
         self.device = device
-        self.pos_encoder = PositionalEncoding(ninp, dropout)
-        encoder_layers = TransformerEncoderLayer(ninp, nhead, nhid, dropout)
-        self.transformer_encoder = TransformerEncoder(encoder_layers, nlayers)
-        self.encoder = nn.Embedding(ntoken, ninp)
         self.ninp = ninp
-        self.decoder = nn.Linear(ninp, ntoken)
-
+        
+        self.src_embedding = nn.Embedding(ntoken, ninp)
+        self.src_pos_encoder = PositionalEncoding(ninp, dropout)
+        self.trg_embedding = nn.Embedding(ntoken, ninp)
+        self.trg_pos_encoder = PositionalEncoding(ninp, dropout)
+        
+        self.transformer = nn.Transformer(
+            ninp,
+            nhead,
+            n_enc,
+            n_dec,
+            forward_expansion,
+            dropout,
+        )
+        
+        self.fc_out = nn.Linear(ninp, ntoken)
         self.init_weights()
 
     def generate_square_subsequent_mask(self, sz):
@@ -38,7 +57,7 @@ class TransformerModel(nn.Module):
         mask = mask.float().masked_fill(mask == 0, float('-inf')).masked_fill(mask == 1, float(0.0))
         return mask
 
-    def make_src_pad_mask(self, src):
+    def make_pad_mask(self, src):
         pad_mask = src.transpose(0, 1) == self.src_pad_idx
         #pad_mask = pad_mask.float().masked_fill(pad_mask == True, float('-inf')).masked_fill(pad_mask == False, float(0.0))
         return pad_mask.to(self.device)
@@ -49,17 +68,25 @@ class TransformerModel(nn.Module):
         self.decoder.bias.data.zero_()
         self.decoder.weight.data.uniform_(-initrange, initrange)
 
-    def forward(self, src, src_mask):
-        src_padding_mask = self.make_src_pad_mask(src)
-        #print(src_padding_mask)
+    def forward(self, src, trg, trg_mask):
+        src_padding_mask = self.make_pad_mask(src)
+        tgt_padding_mask = self.make_pad_mask(trg)
+        
         src = self.encoder(src) * math.sqrt(self.ninp)
+        trg = self.encoder(trg) * math.sqrt(self.ninp)
         src = self.pos_encoder(src)
-        output = self.transformer_encoder(src, src_mask, src_padding_mask)
-        #output = self.transformer_encoder(src, src_mask)
-        output = self.decoder(output)
-        #print(output)
-        return output
+        trg = self.pos_encoder(trg)
+        
+        output = self.transformer(src, 
+                                  trg, 
+                                  tgt_mask=trg_mask, 
+                                  src_key_padding_mask=src_padding_mask, 
+                                  tgt_key_padding_mask=tgt_padding_mask)
 
+        output = self.fc_out(output)
+        return output
+    
+    
 # POSITIONAL ENCODING
 class PositionalEncoding(nn.Module):
 
@@ -79,7 +106,33 @@ class PositionalEncoding(nn.Module):
         x = x + self.pe[:x.size(0), :]
         return self.dropout(x)
 
-def train(model, vocab, train_data, criterion, optimizer, scheduler, epoch, bptt, device):
+
+def get_batch(source, i, bptt):
+    '''
+
+    Parameters
+    ----------
+    source : pytorch Tensor
+        source of batched data.
+    i : integer
+        number of the batch to be selected.
+
+    Returns
+    -------
+    data : pytorch Tensor
+        batch of data of size [bptt x bsz].
+    target : pytorch Tensor
+        same as data but sequences are shifted by one token, 
+        of size [bptt x bsz].
+
+    '''
+    seq_len = min(bptt, len(source) - 1 - i)
+    data = source[i:i+seq_len] # input 
+    target = source[i+1:i+1+seq_len]#.view(-1) # target (same as input but shifted by 1)
+
+    return data, target
+
+def train(model, vocab, train_data, criterion, optimizer, bptt, device, epoch, scheduler):
     '''
 
     Parameters
@@ -108,11 +161,12 @@ def train(model, vocab, train_data, criterion, optimizer, scheduler, epoch, bptt
     for batch, i in enumerate(range(0, train_data.size(0) - 1, bptt)):
         data, targets = get_batch(train_data, i, bptt)
         optimizer.zero_grad()
-        if data.size(0) != bptt:
-            src_mask = model.generate_square_subsequent_mask(data.size(0)).to(device)
         
-        output = model(data, src_mask) 
-        loss = criterion(output.view(-1, ntokens), targets)
+        if targets.size(0) != bptt:
+            src_mask = model.generate_square_subsequent_mask(targets.size(0)).to(device)
+        
+        output = model(data, targets, src_mask) 
+        loss = criterion(output.view(-1, ntokens), targets.view(-1))
         
         loss.backward()
         torch.nn.utils.clip_grad_norm_(model.parameters(), 0.5)
@@ -132,7 +186,7 @@ def train(model, vocab, train_data, criterion, optimizer, scheduler, epoch, bptt
             total_loss = 0
             start_time = time.time()
 
-def evaluate(eval_model, data_source, vocab, criterion, bptt, device):
+def evaluate(eval_model, data_source, vocab, bptt, device, criterion):
     '''
 
     Parameters
@@ -163,29 +217,5 @@ def evaluate(eval_model, data_source, vocab, criterion, bptt, device):
             output_flat = output.view(-1, ntokens)
             total_loss += len(data) * criterion(output_flat, targets).item()
     return total_loss / (len(data_source) - 1)
-
-
-def get_batch(source, i, bptt):
-    '''
-
-    Parameters
-    ----------
-    source : pytorch Tensor
-        source of batched data.
-    i : integer
-        number of the batch to be selected.
-
-    Returns
-    -------
-    data : pytorch Tensor
-        batch of data of size [bptt x bsz].
-    target : pytorch Tensor
-        same as data but sequences are shifted by one token, 
-        of size [bptt x bsz].
-
-    '''
-    seq_len = min(bptt, len(source) - 1 - i)
-    data = source[i:i+seq_len] # input 
-    target = source[i+1:i+1+seq_len].view(-1) # target (same as input but shifted by 1)
-
-    return data, target
+    
+    
