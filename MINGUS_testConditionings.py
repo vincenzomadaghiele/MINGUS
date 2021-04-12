eba7d1e0a8cb3b78641b38fefad628aa425e6f7c
Vincenzo Madaghiele <vincenzo.madaghiele@gmail.com>
2021-04-12 15:38:39 +0200

    second training

--- MINGUS_testConditionings.py
+++ MINGUS_testConditionings.py
@@ -57,7 +57,7 @@ if __name__ == '__main__':
             POSSIBLE_CONDS.append(COND)
     #print(len(POSSIBLE_CONDS))
 
-    for CONDITIONING in POSSIBLE_CONDS[:10]:
+    for CONDITIONING in POSSIBLE_CONDS[10:20]:
 
         #%% PITCH MODEL TRAINING
         
